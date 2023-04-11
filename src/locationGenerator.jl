fd99e407b3a23868c2ce54fabaacfb849d6eb62e
Alessandro Floris <floris.alessandro99@gmail.com>
2023-04-11 16:30:40 +0200

    testing with a small size map

--- src/locationGenerator.jl
+++ src/locationGenerator.jl
@@ -1,5 +1,28 @@
-module locationGenerator
+using OpenStreetMapX
+using StableRNGs
 
-greet() = print("Hello World!")
+map_path = (@__DIR__) * "/../data/cagliariMap.osm"
+#map_path = (@__DIR__) * "/../data/map.osm"
+map_data = get_map_data(map_path)
 
-end # module
+println("The map contains $(length(map_data.nodes)) nodes")
+
+rng = StableRNGs.StableRNG(1234)
+
+pA = generate_point_in_bounds(rng, map_data)
+pB = generate_point_in_bounds(rng, map_data)
+
+println("puntoA: $pA")
+println("puntoB: $pB")
+
+pointA = point_to_nodes(pA, map_data)
+pointB = point_to_nodes(pB, map_data)
+
+println("puntoA: $pointA")
+println("puntoB: $pointB")
+
+sr = shortest_route(map_data, pointA, pointB; routing = :dijkstra)
+
+println(sr)
+
+println(map_data.w)
