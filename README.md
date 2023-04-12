767a7f14f10a6953ffdf3544d0a16dd8ef638008
Alessandro Floris <floris.alessandro99@gmail.com>
2023-04-12 11:44:41 +0200

    getting shortest route between two points of the map

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # locationGenerator
+
+Dato un file OSM che contiene delle informazioni spaziali riguardo una certa zona 
+geografica si occupa di creare una matrice delle distanze e una serie di punti (utilizzando
+il p-median algorithm).
+
+Per la gestione dei dati spaziali utilizziamo OpenStreetMapX.
+
+## Cose da fare 
+
+- Dato un file OSM trasformarlo in un grafo stradale
+- Prendere due nodi random che fanno parte del grafo e calcolare lo shortest path tra i due 
--- src/locationGenerator.jl
+++ src/locationGenerator.jl
@@ -1,28 +1,35 @@
 using OpenStreetMapX
-using StableRNGs
 
+# TODO: far inserire da linea di comando il path del file .osm da processare.
+# Il file dovrà stare nella dir "data". 
 map_path = (@__DIR__) * "/../data/cagliariMap.osm"
-#map_path = (@__DIR__) * "/../data/map.osm"
-map_data = get_map_data(map_path)
 
-println("The map contains $(length(map_data.nodes)) nodes")
+# Restituisce un oggetto di tipo MapData che contiene codificati tutti i dati contenuti 
+# nel file .osm.
+# Questo oggetto viene utilizzato per l'analisi dei dati spaziali.
+map = get_map_data(map_path) 
 
-rng = StableRNGs.StableRNG(1234)
+# Cose da tenere a mente rispetto alla struttura di un oggetto MapData:
+# map.nodes è un dizionario di nodi, ogniuno dei quali rappresenta un oggetto nella mappa (coordinate in ENU)
+# map.g è un grafo che rappresenta la rete stradale
+# map.v è un dizionario che per ogni nodo associa un vertice del grafo stradale 
+# map.n è un vettore che contiene gli id di tutti i nodi
+# ...
 
-pA = generate_point_in_bounds(rng, map_data)
-pB = generate_point_in_bounds(rng, map_data)
+node1 = map.n[1]
+node2 = map.n[36]
 
-println("puntoA: $pA")
-println("puntoB: $pB")
+node1_cord = LLA(map.nodes[node1], map.bounds)
+node2_cord = LLA(map.nodes[node2], map.bounds)
 
-pointA = point_to_nodes(pA, map_data)
-pointB = point_to_nodes(pB, map_data)
+println("$node1 -> $node1_cord")
+println("$node2 -> $node2_cord")
 
-println("puntoA: $pointA")
-println("puntoB: $pointB")
+# Calcola il tragitto più breve tra due nodi facenti parte della mappa.
+# Restituisce:
+# - una lista dei nodi da attraversare
+# - la distanza da percorrere (in metri)
+# - il tempo di percorrenza (in secondi)
+sr, distance, time = shortest_route(map, node1, node2)
 
-sr = shortest_route(map_data, pointA, pointB; routing = :dijkstra)
-
-println(sr)
-
-println(map_data.w)
+println("Distance: $distance, Time: $time")
